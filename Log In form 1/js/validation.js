e8f2550224b9345df8afcbfa01d3d6eb14f080cd
Charukirti Chougule <64370660+Charukirticc@users.noreply.github.com>
2022-07-17 11:32:23 +0530

    Add files via upload

---
+++ Log In form 1/js/validation.js
@@ -0,0 +1,9 @@
+let uname = document.getElementById('uname');
+let upass = document.getElementById('upass');
+let logBtn = document.getElementById('logBtn');
+
+logBtn.addEventListener('click',()=>{
+    if(uname == 'Charukirti'){
+        console.log("Username is Correct");
+    }
+})
